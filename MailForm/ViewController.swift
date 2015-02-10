ce91b5a64a37f9bca16dd08b366f82338558c0a3
Sambulo Senda <sambulosendas@gmail.com>
2015-02-10 16:00:07 +0000

    Added mailComposeController
    Added mailComposeController

--- MailForm/ViewController.swift
+++ MailForm/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import MessageUI // allows you to perform some tasks
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , MFMailComposeViewControllerDelegate{
 
+    @IBOutlet weak var Subject: UITextField!
+    @IBOutlet weak var Body: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,5 +25,47 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func SendEmail(sender: AnyObject) {
+        var SubjectText = "This is Sambulo"
+        SubjectText += Subject.text
+        
+        var MessageBody = Body
+        
+        var toRecipients = ["sambulosenda@gmail.com"]
+        
+        var mc : MFMailComposeViewController = MFMailComposeViewController()
+        mc.setSubject(SubjectText)
+        mc.setMessageBody(MessageBody.text, isHTML: false)
+        mc.setToRecipients(toRecipients)
+        
+        self.presentViewController(mc, animated: true, completion: nil)
+    }
+    
+    // Fix the send and cancel from the user.
+    
+    
+    func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!) {
+        switch result.value{
+            
+            
+            case MFMailComposeResultCancelled.value:
+            NSLog("Mail Cancelled")
+            
+        case MFMailComposeResultSaved.value:
+            NSLog("Mail Saved")
+            
+        case MFMailComposeResultSent.value:
+            NSLog("Mail Sent")
+            
+        case MFMailComposeResultFailed.value:
+            NSLog("Mail Sent Failure: %@",[error.localizedDescription])
+        default:
+            break
+        
+        }
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
 }
 
