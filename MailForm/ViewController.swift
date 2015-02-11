64b3689ee580f2181bd71e5829f0bd1a7f9a104a
Sambulo Senda <sambulosendas@gmail.com>
2015-02-11 10:49:18 +0000

    MailCompose Changed
    Changed the mail Composer controller

--- MailForm/ViewController.swift
+++ MailForm/ViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Sambulo Senda on 10/02/2015.
 //  Copyright (c) 2015 Sambulo Senda. All rights reserved.
 //
-
 import UIKit
 import MessageUI // allows you to perform some tasks
 
@@ -17,22 +16,17 @@ class ViewController: UIViewController , MFMailComposeViewControllerDelegate{
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
         superclass
     }
-
-
     @IBAction func SendEmail(sender: AnyObject) {
         var SubjectText = "This is Sambulo"
         SubjectText += Subject.text
         
         var MessageBody = Body
-        
         var toRecipients = ["sambulosenda@gmail.com"]
-        
         var mc : MFMailComposeViewController = MFMailComposeViewController()
         mc.setSubject(SubjectText)
         mc.setMessageBody(MessageBody.text, isHTML: false)
@@ -41,31 +35,29 @@ class ViewController: UIViewController , MFMailComposeViewControllerDelegate{
         self.presentViewController(mc, animated: true, completion: nil)
     }
     
-    // Fix the send and cancel from the user.
-    
-    
-    func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!) {
-        switch result.value{
-            
-            
-            case MFMailComposeResultCancelled.value:
-            NSLog("Mail Cancelled")
-            
+
+      //--------------------------------------------------------//
+      // Fix the send and cancel from the user.
+      //--------------------------------------------------------//
+      //--------------------------------------------------------//
+      //--------------------------------------------------------//
+    // Email Delegate
+    func mailComposeController(controller:MFMailComposeViewController, didFinishWithResult result:MFMailComposeResult, error:NSError) {
+        
+        switch result.value {
+        case MFMailComposeResultCancelled.value:
+            println("Mail cancelled")
         case MFMailComposeResultSaved.value:
-            NSLog("Mail Saved")
-            
+            println("Mail saved")
         case MFMailComposeResultSent.value:
-            NSLog("Mail Sent")
-            
+            println("Mail sent")
         case MFMailComposeResultFailed.value:
-            NSLog("Mail Sent Failure: %@",[error.localizedDescription])
+            println("Mail sent failure: \(error.localizedDescription)")
         default:
             break
-        
         }
-        
-        self.dismissViewControllerAnimated(true, completion: nil)
+        self.dismissViewControllerAnimated(false, completion: nil)
         
     }
+    
 }
-
